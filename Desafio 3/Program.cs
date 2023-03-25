56eaf12a6f1d0ed19593d72567c61c99b035c1f2
ALejo20032023 <jmorales.ipedep@gmail.com>
2023-03-24 21:25:48 -0600

    Se Agrego

---
+++ Desafio 3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace desafio 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = SumarEnterosDesdeConsola();
+            Console.WriteLine("Resultado de la suma es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
